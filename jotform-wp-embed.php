a9008c5906999f38d77a82f8e1233a58fb11adb5
Derya Yeşilyurt <derya@jotform.com>
2022-12-21 19:44:15 +0300

    Version 1.3.0

--- jotform-wp-embed.php
+++ jotform-wp-embed.php
@@ -4,7 +4,7 @@
  * Description:       Securely embed online forms in your WordPress website.
  * Requires at least: 3.3
  * Requires PHP:      5.6
- * Version:           1.2.5
+ * Version:           1.3.0
  * Author:            Jotform
  * Author URI:        https://www.jotform.com
  * License:           GNU General Public License v3
